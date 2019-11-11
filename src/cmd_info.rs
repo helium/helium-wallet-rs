edf1f8f629c2a18660892dcf8a92fc6b6a872f56
Marc Nijdam <marc@helium.com>
2019-11-11 08:21:34 -0800

    Add qr code printing suppor for a single wallet

--- src/cmd_info.rs
+++ src/cmd_info.rs
@@ -3,22 +3,27 @@ use crate::{
     wallet::Wallet,
 };
 use prettytable::Table;
+use qr2term::print_qr;
 use std::{error::Error, fs, path::PathBuf, result::Result};
 
-pub fn cmd_info(files: Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
+pub fn cmd_info(files: Vec<PathBuf>, qr_code: bool) -> Result<(), Box<dyn Error>> {
     let mut wallets = Vec::with_capacity(files.len());
     for file in files {
         let mut reader = fs::File::open(&file)?;
         let enc_wallet = Wallet::read(&mut reader)?;
         wallets.push(enc_wallet);
     }
-    print_wallets(wallets);
+    if wallets.len() == 1 && qr_code {
+        let address = wallets[0].public_key().to_b58()?;
+        print_qr(&address)?;
+    } else {
+        print_wallets(wallets);
+    }
     Ok(())
 }
 
 fn print_wallets(wallets: Vec<Wallet>) {
     let mut table = Table::new();
-
     table.add_row(row!["Address", "Sharded"]);
     for wallet in wallets {
         let address = wallet

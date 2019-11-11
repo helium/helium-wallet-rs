aac0c3202d897c122f31a777917bec2a90332e1e
Marc Nijdam <marc@helium.com>
2019-11-11 14:53:50 -0800

    Address review feedback

--- src/cmd_info.rs
+++ src/cmd_info.rs
@@ -1,21 +1,27 @@
 use crate::{
+    result::Result,
     traits::{ReadWrite, B58},
     wallet::Wallet,
 };
 use prettytable::Table;
 use qr2term::print_qr;
-use std::{error::Error, fs, path::PathBuf, result::Result};
+use std::{fs, path::PathBuf};
 
-pub fn cmd_info(files: Vec<PathBuf>, qr_code: bool) -> Result<(), Box<dyn Error>> {
+pub fn cmd_info(files: Vec<PathBuf>, qr_code: bool) -> Result {
     let mut wallets = Vec::with_capacity(files.len());
     for file in files {
         let mut reader = fs::File::open(&file)?;
         let enc_wallet = Wallet::read(&mut reader)?;
         wallets.push(enc_wallet);
     }
-    if wallets.len() == 1 && qr_code {
-        let address = wallets[0].public_key().to_b58()?;
-        print_qr(&address)?;
+
+    if qr_code {
+        if wallets.len() == 1 {
+            let address = wallets[0].public_key().to_b58()?;
+            print_qr(&address)?;
+        } else {
+            return Err("Only one wallet allowed for QR code option".into());
+        }
     } else {
         print_wallets(wallets);
     }

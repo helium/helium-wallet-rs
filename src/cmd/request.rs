95d95a6337078738ae6aafa8f246d54d5b192d80
Marc Nijdam <marc_nijdam@mac.com>
2020-08-28 17:55:48 -0600

    Add request burn command

--- src/cmd/request.rs
+++ src/cmd/request.rs
@@ -10,13 +10,27 @@ use structopt::StructOpt;
 /// Construct various request (like payment) in a QR code
 pub enum Cmd {
     Payment(Payment),
+    Burn(Burn),
 }
 
 #[derive(Debug, StructOpt)]
-/// Construct a payment request in a QR code
+/// Construct a payment request in a QR code with optional amount
 pub struct Payment {
     #[structopt(long)]
     /// Amount of HNT to request
+    amount: Option<Hnt>,
+}
+
+#[derive(Debug, StructOpt)]
+/// Construct a token burn request in a QR code with optional amount and memo
+/// fields.
+pub struct Burn {
+    /// Memo field to include. Provide as a base64 encoded string
+    #[structopt(long)]
+    memo: Option<String>,
+
+    /// Amount of HNT to burn to DC
+    #[structopt(long)]
     amount: Option<Hnt>,
 }
 
@@ -24,6 +38,7 @@ impl Cmd {
     pub fn run(&self, opts: Opts) -> Result {
         match self {
             Cmd::Payment(cmd) => cmd.run(opts),
+            Cmd::Burn(cmd) => cmd.run(opts),
         }
     }
 }
@@ -38,6 +53,25 @@ impl Payment {
         });
         if self.amount.is_some() {
             request["amount"] = self.amount.unwrap().to_string().into();
+        }
+        print_request(&request, opts.format)
+    }
+}
+
+impl Burn {
+    pub fn run(&self, opts: Opts) -> Result {
+        let wallet = load_wallet(opts.files)?;
+
+        let mut request = json!({
+            "type": "dc_burn",
+            "address": wallet.address()?,
+        });
+        if self.amount.is_some() {
+            request["amount"] = self.amount.unwrap().to_string().into();
+        }
+        if self.memo.is_some() {
+            let value = self.memo.as_ref().unwrap();
+            request["memo"] = value.clone().into();
         }
         print_request(&request, opts.format)
     }

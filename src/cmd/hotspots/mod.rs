5de86bfa267515524f6040e29a7384c6ecf6ab28
Louis Thiery <thiery.louis@gmail.com>
2020-12-16 08:51:18 -0800

    remove score from hotspots

--- src/cmd/hotspots/mod.rs
+++ src/cmd/hotspots/mod.rs
@@ -52,7 +52,7 @@ fn print_results(results: Vec<(String, Result<Vec<Hotspot>>)>, format: OutputFor
             let mut table = Table::new();
             table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
             table.set_titles(row![
-                "Address", "Name", "Location", "City", "State", "Score"
+                "Address", "Name", "Location", "City", "State"
             ]);
 
             for (address, result) in results {
@@ -75,8 +75,7 @@ fn print_results(results: Vec<(String, Result<Vec<Hotspot>>)>, format: OutputFor
                                 hotspot
                                     .geocode
                                     .short_state
-                                    .unwrap_or_else(|| "unknown".to_string()),
-                                hotspot.score
+                                    .unwrap_or_else(|| "unknown".to_string())
                             ]);
                         }
                     }
@@ -106,9 +105,7 @@ fn print_results(results: Vec<(String, Result<Vec<Hotspot>>)>, format: OutputFor
                                 hotspot
                                     .geocode
                                     .short_state
-                                .unwrap_or_else(|| "unknown".to_string()),
-                            "score":
-                                hotspot.score
+                                .unwrap_or_else(|| "unknown".to_string())
                         }))
                     }
                 };

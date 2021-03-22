0aa7100d01abea6c2c5bb91eef4725f3f6a2a35e
Louis Thiery <louis@helium.com>
2021-03-22 12:55:35 -0700

    OUI update txn onto async PR (#99)

--- src/cmd/oui/create.rs
+++ src/cmd/oui/create.rs
@@ -1,19 +1,9 @@
 use crate::{
-    cmd::*,
-    keypair::PublicKey,
-    result::{anyhow, Result},
-    traits::{TxnEnvelope, TxnFee, TxnSign, TxnStakingFee, B64},
+    cmd::oui::*,
+    traits::{TxnEnvelope, TxnFee, TxnSign, TxnStakingFee},
 };
-use helium_api::ouis;
-use serde_json::json;
-use std::convert::TryInto;
-
-/// Create or update an OUI
-#[derive(Debug, StructOpt)]
-pub enum Cmd {
-    Create(Create),
-    Submit(Submit),
-}
+use helium_api::{ouis, Client};
+use structopt::StructOpt;
 
 /// Allocates an Organizational Unique Identifier (OUI) which
 /// identifies endpoints for packets to sent to The transaction is not
@@ -24,8 +14,18 @@ pub struct Create {
     #[structopt(long = "address", short = "a", number_of_values(1))]
     addresses: Vec<PublicKey>,
 
-    /// Initial device membership filter in base64 encoded form
+    /// Optionally indicate last OUI. Wallet will determine
+    /// this using API otherwise.
     #[structopt(long)]
+    last_oui: Option<u64>,
+
+    /// Initial device membership filter in base64 encoded form.
+    /// Dummy filter default is given, but OUI addresses may
+    /// overwrite this at any time.
+    #[structopt(
+        long,
+        default_value = "wVwCiewtCpEKAAAAAAAAAAAAcCK3fwAAAAAAAAAAAABI7IQOAHAAAAAAAAAAAAAAAQAAADBlAAAAAAAAAAAAADEAAAA2AAAAOgAAAA"
+    )]
     filter: String,
 
     /// Requested subnet size. Must be a value between 8 and 65,536
@@ -46,31 +46,6 @@ pub struct Create {
     commit: bool,
 }
 
-/// Submits a given base64 oui transaction to the API. This command
-/// can be used when this wallet is not the payer of the oui
-/// transaction.
-#[derive(Debug, StructOpt)]
-pub struct Submit {
-    /// Base64 encoded transaction to submit.
-    #[structopt(name = "TRANSACTION")]
-    transaction: String,
-
-    /// Commit the payment to the API. If the staking server is used
-    /// as the payer the transaction is first submitted to the staking
-    /// server for signing and the result submitted ot the API.
-    #[structopt(long)]
-    commit: bool,
-}
-
-impl Cmd {
-    pub async fn run(&self, opts: Opts) -> Result {
-        match self {
-            Cmd::Create(cmd) => cmd.run(opts).await,
-            Cmd::Submit(cmd) => cmd.run(opts).await,
-        }
-    }
-}
-
 impl Create {
     pub async fn run(&self, opts: Opts) -> Result {
         let password = get_password(false)?;
@@ -78,13 +53,19 @@ impl Create {
         let keypair = wallet.decrypt(password.as_bytes())?;
         let wallet_key = keypair.public_key();
 
-        let api_client = Client::new_with_base_url(api_url(wallet.public_key.network));
+        let client = Client::new_with_base_url(api_url(wallet.public_key.network));
+
+        let oui = if let Some(oui) = self.last_oui {
+            oui
+        } else {
+            ouis::last(&client).await?.oui
+        };
 
         let mut txn = BlockchainTxnOuiV1 {
             addresses: map_addresses(self.addresses.clone(), |v| v.to_vec())?,
             owner: keypair.public_key().into(),
             payer: self.payer.as_ref().map_or(vec![], |v| v.into()),
-            oui: ouis::last(&api_client).await?.oui,
+            oui,
             fee: 0,
             staking_fee: 1,
             owner_signature: vec![],
@@ -92,16 +73,19 @@ impl Create {
             requested_subnet_size: self.subnet_size,
             filter: base64::decode(&self.filter)?,
         };
-        let txn_fees = get_txn_fees(&api_client).await?;
-        txn.fee = txn.txn_fee(&txn_fees)?;
-        txn.staking_fee = txn.txn_staking_fee(&txn_fees)?;
+
+        let fees = &get_txn_fees(&client).await?;
+
+        txn.fee = txn.txn_fee(&fees)?;
+        txn.staking_fee = txn.txn_staking_fee(&fees)?;
+
         txn.owner_signature = txn.sign(&keypair)?;
         let envelope = txn.in_envelope();
 
         match self.payer.as_ref() {
             key if key == Some(&wallet_key) || key.is_none() => {
                 // Payer is the wallet submit if ready to commit
-                let status = maybe_submit_txn(self.commit, &api_client, &envelope).await?;
+                let status = maybe_submit_txn(self.commit, &client, &envelope).await?;
                 print_txn(&txn, &envelope, &status, opts.format)
             }
             _ => {
@@ -109,20 +93,6 @@ impl Create {
                 // can't commit this transaction but we can display it
                 print_txn(&txn, &envelope, &None, opts.format)
             }
-        }
-    }
-}
-
-impl Submit {
-    pub async fn run(&self, opts: Opts) -> Result {
-        let envelope = BlockchainTxn::from_b64(&self.transaction)?;
-        if let Some(Txn::Oui(t)) = envelope.txn.clone() {
-            let api_url = api_url(PublicKey::from_bytes(&t.owner)?.network);
-            let api_client = helium_api::Client::new_with_base_url(api_url);
-            let status = maybe_submit_txn(self.commit, &api_client, &envelope).await?;
-            print_txn(&t, &envelope, &status, opts.format)
-        } else {
-            Err(anyhow!("Invalid OUI transaction"))
         }
     }
 }
@@ -137,8 +107,8 @@ fn print_txn(
         OutputFormat::Table => {
             ptable!(
                 ["Key", "Value"],
-                ["Requested OUI", txn.oui + 1],
-                ["Reqeuested Subnet Size", txn.requested_subnet_size],
+                ["Previous OUI", txn.oui],
+                ["Requested Subnet Size", txn.requested_subnet_size],
                 [
                     "Addresses",
                     map_addresses(txn.addresses.clone(), |v| v.to_string())?.join("\n")
@@ -150,7 +120,7 @@ fn print_txn(
         }
         OutputFormat::Json => {
             let table = json!({
-                "requested_oui": txn.oui + 1,
+                "previous_oui": txn.oui,
                 "addresses": map_addresses(txn.addresses.clone(), |v| v.to_string())?,
                 "requested_subnet_size": txn.requested_subnet_size,
                 "hash": status_json(status),
@@ -160,18 +130,4 @@ fn print_txn(
             print_json(&table)
         }
     }
-}
-
-fn map_addresses<F, R>(addresses: Vec<impl TryInto<PublicKey>>, f: F) -> Result<Vec<R>>
-where
-    F: Fn(PublicKey) -> R,
-{
-    let results: Result<Vec<R>> = addresses
-        .into_iter()
-        .map(|v| match v.try_into() {
-            Ok(public_key) => Ok(f(public_key)),
-            Err(_err) => Err(anyhow!("failed to convert to public key")),
-        })
-        .collect();
-    results
 }
---
+++ src/cmd/oui/update.rs
@@ -0,0 +1,208 @@
+use crate::{
+    cmd::oui::*,
+    traits::{TxnEnvelope, TxnFee, TxnSign, TxnStakingFee, B64},
+};
+use helium_api::{ouis, pending_transactions::PendingTxnStatus, Client};
+use serde_json::json;
+use structopt::StructOpt;
+
+/// Updates an organizational OUI. The transaction is not
+/// submitted to the system unless the '--commit' option is given.
+#[derive(Debug, StructOpt)]
+pub enum Update {
+    /// The address(es) of the router to send packets to. This will overwrite any previous
+    /// router(s)
+    Routers(Routers),
+    /// Create new or update Xor filter by hand. This is normally done by the OUI operator,
+    /// but may be done by hand here.
+    Xor(Xor),
+    /// Requested additional subnet size. Must be a value between 8 and 65,536
+    /// and a power of two.
+    RequestSubset(RequestSubnet),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Routers {
+    /// OUI to update
+    #[structopt(required = true, long)]
+    pub oui: u32,
+    /// The address(es) of the router to send packets to
+    #[structopt(required = true, long = "address", short = "a", number_of_values(1))]
+    pub addresses: Vec<PublicKey>,
+    /// Which OUI nonce this transaction has
+    #[structopt(long)]
+    pub nonce: Option<u64>,
+    /// Commit the transaction to the API
+    #[structopt(long)]
+    pub commit: bool,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Xor {
+    /// If less than 5 filters have been defined,
+    /// you can create an additional Xor
+    New(xor::New),
+    /// Overwrite an existing Xor filter
+    Update(xor::Update),
+}
+
+pub mod xor {
+    use super::super::StructOpt;
+    #[derive(Debug, StructOpt)]
+    /// Update an already defined Xor
+    pub struct Update {
+        /// OUI to update
+        #[structopt(required = true, long)]
+        pub oui: u32,
+        /// select which Xor to update
+        #[structopt(required = true, long)]
+        pub index: u32,
+        /// 100kb or less
+        #[structopt(required = true, long)]
+        pub filter: String,
+        /// Which OUI nonce this transaction has
+        #[structopt(long)]
+        pub nonce: Option<u64>,
+        /// Commit the transaction to the API
+        #[structopt(long)]
+        pub commit: bool,
+    }
+    #[derive(Debug, StructOpt)]
+    pub struct New {
+        /// OUI to update
+        #[structopt(required = true, long)]
+        pub oui: u32,
+        /// 100kb or less
+        #[structopt(required = true, long)]
+        pub filter: String,
+        /// Which OUI nonce this transaction has
+        #[structopt(long)]
+        pub nonce: Option<u64>,
+        /// Commit the transaction to the API
+        #[structopt(long)]
+        pub commit: bool,
+    }
+}
+
+#[derive(Debug, StructOpt)]
+pub struct RequestSubnet {
+    /// OUI to update
+    #[structopt(required = true, long)]
+    pub oui: u32,
+    #[structopt(required = true, long)]
+    pub size: u32,
+    /// Which OUI nonce this transaction has
+    #[structopt(long)]
+    pub nonce: Option<u64>,
+    /// Commit the transaction to the API
+    #[structopt(long)]
+    pub commit: bool,
+}
+
+impl Update {
+    pub async fn run(&self, opts: Opts) -> Result {
+        let password = get_password(false)?;
+        let wallet = load_wallet(opts.files)?;
+        let keypair = wallet.decrypt(password.as_bytes())?;
+        let client = Client::new_with_base_url(api_url(wallet.public_key.network));
+
+        let (oui, commit, nonce, update) = match self {
+            Update::Routers(routers) => (
+                routers.oui,
+                routers.commit,
+                routers.nonce,
+                blockchain_txn_routing_v1::Update::UpdateRouters(UpdateRouters {
+                    router_addresses: map_addresses(routers.addresses.clone(), |v| v.to_vec())?,
+                }),
+            ),
+            Update::Xor(xor) => match xor {
+                Xor::New(filter) => (
+                    filter.oui,
+                    filter.commit,
+                    filter.nonce,
+                    blockchain_txn_routing_v1::Update::NewXor(base64::decode(&filter.filter)?),
+                ),
+                Xor::Update(update) => (
+                    update.oui,
+                    update.commit,
+                    update.nonce,
+                    blockchain_txn_routing_v1::Update::UpdateXor(UpdateXor {
+                        index: update.index,
+                        filter: base64::decode(&update.filter)?,
+                    }),
+                ),
+            },
+            Update::RequestSubset(size) => (
+                size.oui,
+                size.commit,
+                size.nonce,
+                blockchain_txn_routing_v1::Update::RequestSubnet(size.size),
+            ),
+        };
+
+        let mut txn = BlockchainTxnRoutingV1 {
+            // the type in the proto diverges from the more common u64
+            oui: oui as u32,
+            owner: keypair.public_key().into(),
+            fee: 0,
+            signature: vec![],
+            staking_fee: 0,
+            update: Some(update),
+            nonce: if let Some(nonce) = nonce {
+                nonce
+            } else {
+                ouis::get(&client, oui.into()).await?.nonce + 1
+            },
+        };
+        let fees = get_txn_fees(&client).await?;
+        txn.fee = txn.txn_fee(&fees)?;
+        txn.staking_fee = txn.txn_staking_fee(&fees)?;
+        txn.signature = txn.sign(&keypair)?;
+        let envelope = txn.in_envelope();
+
+        let status = maybe_submit_txn(commit, &client, &envelope).await?;
+        print_txn(&txn, &envelope, &status, opts.format)
+    }
+}
+
+fn print_txn(
+    txn: &BlockchainTxnRoutingV1,
+    envelope: &BlockchainTxn,
+    status: &Option<PendingTxnStatus>,
+    format: OutputFormat,
+) -> Result {
+    let update = match txn.update.as_ref().unwrap() {
+        blockchain_txn_routing_v1::Update::UpdateRouters(txn) => {
+            let mut str = String::from("Routing ");
+            let addr = map_addresses(txn.router_addresses.clone(), |v| v.to_string())?;
+            str.extend(addr);
+            str
+        }
+        blockchain_txn_routing_v1::Update::NewXor(_) => "NewXor".into(),
+        blockchain_txn_routing_v1::Update::UpdateXor(txn) => format!("Update Xor {}", txn.index),
+        blockchain_txn_routing_v1::Update::RequestSubnet(size) => {
+            format!("Request subnet of size {}", size)
+        }
+    };
+
+    match format {
+        OutputFormat::Table => {
+            ptable!(
+                ["Key", "Value"],
+                ["Last OUI", txn.oui],
+                ["Update", update],
+                ["Hash", status_str(status)]
+            );
+            print_footer(status)
+        }
+        OutputFormat::Json => {
+            let table = json!({
+                "last_oui": txn.oui,
+                "update": update,
+                "hash": status_json(status),
+                "txn": envelope.to_b64()?,
+            });
+            print_json(&table)
+        }
+    }
+}

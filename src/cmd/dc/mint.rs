fde17e52be1e9ec344dfca82dd276ebf31cc412b
Marc Nijdam <marc_nijdam@mac.com>
2023-06-08 15:22:18 -0600

    use payee on dc mint

--- src/cmd/dc/mint.rs
+++ src/cmd/dc/mint.rs
@@ -7,6 +7,9 @@ use crate::{
 
 #[derive(Debug, Clone, clap::Args)]
 /// Burn HNT to Data Credits (DC) from this wallet to given payees wallet.
+///
+/// One of the amount of HNT to burn or the amount of DC expected after the burn
+/// can be specified.
 pub struct Cmd {
     /// Account address to send the resulting DC to. Defaults to the active
     /// wallet.
@@ -31,15 +34,16 @@ impl Cmd {
         let password = get_wallet_password(false)?;
         let wallet = load_wallet(&opts.files)?;
         let client = new_client(&opts.url)?;
-        let keypair = wallet.decrypt(password.as_bytes())?;
 
+        let payee = self.payee.as_ref().unwrap_or(&wallet.public_key);
         let amount = match (self.hnt, self.dc) {
             (Some(hnt), None) => TokenAmount::from_f64(Token::Hnt, hnt),
             (None, Some(dc)) => TokenAmount::from_u64(Token::Dc, dc),
             _ => return Err(anyhow!("Must specify either HNT or DC")),
         };
 
-        let tx = client.mint_dc(amount, &wallet.public_key, keypair)?;
+        let keypair = wallet.decrypt(password.as_bytes())?;
+        let tx = client.mint_dc(amount, payee, keypair)?;
         self.commit.maybe_commit(&tx, &client)
     }
 }

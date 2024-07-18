7ebc1a2f823cf8dbf9dfdde5ef2ed75bb1fa52ff
jeffgrunewald <jeff.grunewald@gmail.com>
2024-07-18 12:41:46 -0400

    make metadata name non-optional

--- helium-lib/src/asset.rs
+++ helium-lib/src/asset.rs
@@ -266,7 +266,7 @@ pub struct AssetContent {
 pub struct AssetMetadata {
     #[serde(default)]
     pub attributes: Vec<AssetMetadataAttribute>,
-    pub name: Option<String>,
+    pub name: String,
     pub symbol: String,
 }
 

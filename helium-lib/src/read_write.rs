bd87fdd9d45408c4c42350bb2b70fbd00b7553ac
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-10-22 14:47:16 -0700

    Move ReadWrite trait to helium-lib
    You can directly read a helium_lib::Keypair from a file that is a solana keypair.

--- helium-lib/src/read_write.rs
+++ helium-lib/src/read_write.rs
@@ -1,29 +1,32 @@
-use crate::result::{bail, Result};
 use helium_crypto::{ecc_compact, ed25519, multisig, KeyType};
 use io::{Read, Write};
 use std::io;
 
+use crate::error::{DecodeError, EncodeError};
+
 pub trait ReadWrite {
-    fn read(reader: &mut dyn Read) -> Result<Self>
+    fn read(reader: &mut dyn Read) -> Result<Self, DecodeError>
     where
         Self: std::marker::Sized;
-    fn write(&self, writer: &mut dyn Write) -> Result;
+    fn write(&self, writer: &mut dyn Write) -> Result<(), EncodeError>;
 }
 
 impl ReadWrite for helium_crypto::PublicKey {
-    fn write(&self, writer: &mut dyn io::Write) -> Result {
+    fn write(&self, writer: &mut dyn io::Write) -> Result<(), EncodeError> {
         Ok(writer.write_all(&self.to_vec())?)
     }
 
-    fn read(reader: &mut dyn Read) -> Result<Self> {
+    fn read(reader: &mut dyn Read) -> Result<Self, DecodeError> {
         let mut data = vec![0u8; 1];
         reader.read_exact(&mut data[0..1])?;
         let key_size = match KeyType::try_from(data[0])? {
             KeyType::Ed25519 => ed25519::PUBLIC_KEY_LENGTH,
             KeyType::EccCompact => ecc_compact::PUBLIC_KEY_LENGTH,
             KeyType::MultiSig => multisig::PUBLIC_KEY_LENGTH,
-            KeyType::Secp256k1 => bail!("Secp256k1 key type unsupported for read."),
-            KeyType::Rsa => bail!("RSA key type unsupported for read."),
+            KeyType::Secp256k1 => Err(DecodeError::other(
+                "Secp256k1 key type unsupported for read.",
+            ))?,
+            KeyType::Rsa => Err(DecodeError::other("RSA key type unsupported for read."))?,
         };
         data.resize(key_size, 0);
         reader.read_exact(&mut data[1..])?;
@@ -31,26 +34,26 @@ impl ReadWrite for helium_crypto::PublicKey {
     }
 }
 
-impl ReadWrite for helium_lib::keypair::Pubkey {
-    fn write(&self, writer: &mut dyn io::Write) -> Result {
+impl ReadWrite for crate::keypair::Pubkey {
+    fn write(&self, writer: &mut dyn io::Write) -> Result<(), EncodeError> {
         writer.write_all(&self.to_bytes())?;
         Ok(())
     }
 
-    fn read(reader: &mut dyn Read) -> Result<helium_lib::keypair::Pubkey> {
-        let mut data = [0u8; helium_lib::keypair::PUBKEY_BYTES];
+    fn read(reader: &mut dyn Read) -> Result<crate::keypair::Pubkey, DecodeError> {
+        let mut data = [0u8; crate::keypair::PUBKEY_BYTES];
         reader.read_exact(&mut data)?;
         Ok(Self::new_from_array(data))
     }
 }
 
-impl ReadWrite for helium_lib::keypair::Keypair {
-    fn write(&self, writer: &mut dyn io::Write) -> Result {
+impl ReadWrite for crate::keypair::Keypair {
+    fn write(&self, writer: &mut dyn io::Write) -> Result<(), EncodeError> {
         writer.write_all(&self.to_bytes())?;
         Ok(())
     }
 
-    fn read(reader: &mut dyn io::Read) -> Result<Self> {
+    fn read(reader: &mut dyn io::Read) -> Result<Self, DecodeError> {
         let mut sk_buf = [0u8; 64];
         reader.read_exact(&mut sk_buf)?;
         Ok(Self::try_from(&sk_buf)?)
@@ -60,7 +63,7 @@ impl ReadWrite for helium_lib::keypair::Keypair {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use helium_lib::keypair::{Keypair, Pubkey, Signer};
+    use crate::keypair::{Keypair, Pubkey, Signer};
     use std::{io::Cursor, str::FromStr};
 
     #[test]

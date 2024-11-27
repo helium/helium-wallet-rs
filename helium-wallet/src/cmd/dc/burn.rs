aaeab218c9becb76735d833ab8b6b8f7022fd5f2
Marc Nijdam <marc_nijdam@mac.com>
2024-11-27 11:42:07 -0400

    remove repeated transasction into a common func
    Introduces mk_transaction_with_blockhash which returns an unsigned transation with the expected (Transaction, height) tuple. This removes all the boiler plate in the *_transaction functions.
    
    Also adjust dc mod to include priority fees for burn, mint, delegate, burn_delegated.

--- helium-wallet/src/cmd/dc/burn.rs
+++ helium-wallet/src/cmd/dc/burn.rs
@@ -17,8 +17,9 @@ impl Cmd {
         let password = get_wallet_password(false)?;
         let keypair = opts.load_keypair(password.as_bytes())?;
         let client = opts.client()?;
+        let transaction_opts = self.commit.transaction_opts();
 
-        let tx = dc::burn(&client, self.dc, &keypair).await?;
+        let (tx, _) = dc::burn(&client, self.dc, &keypair, &transaction_opts).await?;
         print_json(&self.commit.maybe_commit(&tx, &client).await?.to_json())
     }
 }

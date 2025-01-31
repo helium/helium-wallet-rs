6a02ddb1dfb9f2af992ff5eed8ba1cb5e92bf747
Marc Nijdam <marc_nijdam@mac.com>
2025-01-31 07:45:01 -0400

    Temporarily patch cert service mainnet url (#447)

--- helium-lib/src/client.rs
+++ helium-lib/src/client.rs
@@ -23,7 +23,8 @@ pub static SOLANA_URL_DEVNET: &str = "https://solana-rpc.web.test-helium.com?ses
 pub static SOLANA_URL_MAINNET_ENV: &str = "SOLANA_MAINNET_URL";
 pub static SOLANA_URL_DEVNET_ENV: &str = "SOLANA_DEVNET_URL";
 
-pub static CERT_URL_MAINNET: &str = "";
+// TODO: Set to correct CERT service URL when available
+pub static CERT_URL_MAINNET: &str = "https://api.svt.ims.nova.xyz/api/wifi/brownfield/inventory";
 pub static CERT_URL_DEVNET: &str = "https://api.svt.ims.nova.xyz/api/wifi/brownfield/inventory";
 pub static CERT_URL_MAINNET_ENV: &str = "CERT_MAINNET_URL";
 pub static CERT_URL_DEVNET_ENV: &str = "CERT_DEVNET_URL";

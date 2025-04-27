db1e71fd2c1514ae65bb100b55698cdd871f665c
Marc Nijdam <marc_nijdam@mac.com>
2025-04-26 18:46:18 -0700

    Fixes anchor_lang (#471)
    * Use transaction_utils
    
    * Fix anchor by pulling in const_pubkey support

--- helium-lib/src/error.rs
+++ helium-lib/src/error.rs
@@ -33,6 +33,8 @@ pub enum Error {
     Solana(Box<solana_client::client_error::ClientError>),
     #[error("instruction: {0}")]
     Instruction(#[from] solana_sdk::instruction::InstructionError),
+    #[error("transaction: {0}")]
+    Transaction(#[from] solana_transaction_utils::error::Error),
     #[error("message: {0}")]
     Cmopile(#[from] solana_sdk::message::CompileError),
     #[error("signing: {0}")]
@@ -101,8 +103,6 @@ pub enum EncodeError {
     Bincode(#[from] bincode::Error),
     #[error("h3: {0}")]
     H3(#[from] h3o::error::InvalidLatLng),
-    #[error("instruction group too long: {0}")]
-    TooManyInstructions(usize),
     #[error("encode: {0}")]
     Encode(String),
 }
@@ -110,10 +110,6 @@ pub enum EncodeError {
 impl EncodeError {
     pub fn other<S: ToString>(reason: S) -> Self {
         Self::Encode(reason.to_string())
-    }
-
-    pub fn too_many_instructions(len: usize) -> Self {
-        Self::TooManyInstructions(len)
     }
 }
 

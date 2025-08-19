cf8195a10395fbb8c85c9b90734bc93bf9b1329d
Marc Nijdam <marc_nijdam@mac.com>
2025-08-18 21:43:16 -0600

    Add delegated dc burn to “dc burn”

--- helium-wallet/src/cmd/dc/burn.rs
+++ helium-wallet/src/cmd/dc/burn.rs
@@ -1,12 +1,18 @@
 use crate::cmd::*;
-use helium_lib::dc;
+use helium_lib::{dao, dc};
 
 #[derive(Debug, Clone, clap::Args)]
-/// Burn Data Credits (DC) from this wallet into oblivion.
+/// Burn Data Credits (DC) from this wallet or delegated for the given router into oblivion.
 pub struct Cmd {
     /// Amount of DC to burn
     dc: u64,
-
+    /// Subdao to use for delegated burn
+    subdao: Option<dao::SubDao>,
+    /// Router key to burn on behalf of
+    ///
+    /// Note that the wallet keypair must be the burn authority for the router key
+    /// for the burn to succeed
+    router: Option<String>,
     /// Commit the burn
     #[command(flatten)]
     commit: CommitOpts,
@@ -19,7 +25,21 @@ impl Cmd {
         let client = opts.client()?;
         let transaction_opts = self.commit.transaction_opts(&client);
 
-        let (tx, _) = dc::burn(&client, self.dc, &keypair, &transaction_opts).await?;
+        let (tx, _) = match (&self.router, self.subdao) {
+            (Some(router_key), Some(subdao)) => {
+                dc::burn_delegated(
+                    &client,
+                    subdao,
+                    &keypair,
+                    self.dc,
+                    router_key,
+                    &transaction_opts,
+                )
+                .await?
+            }
+            (None, None) => dc::burn(&client, self.dc, &keypair, &transaction_opts).await?,
+            _ => bail!("both router and subdao must be specified"),
+        };
         print_json(&self.commit.maybe_commit(tx, &client).await?.to_json())
     }
 }

4560ab8f7acc2f77eb702067f3a2434f0514dbf8
Connor McKenzie <connora.mckenzie@gmail.com>
2025-11-17 00:03:26 +0000

    Helium-proto-crypto crate created with MsgVerify trait

---
+++ helium-proto-crypto/src/lib.rs
@@ -0,0 +1,30 @@
+use helium_crypto::{PublicKey, Verify};
+use msg_signature::MsgHasSignature;
+
+#[derive(thiserror::Error, Debug)]
+pub enum MsgVerifyError {
+    #[error("prost encode error: {0}")]
+    Prost(#[from] prost::EncodeError),
+
+    #[error("crypto error: {0}")]
+    Crypto(#[from] helium_crypto::Error),
+}
+
+pub trait MsgVerify {
+    fn verify(&self, verifier: &PublicKey) -> Result<(), MsgVerifyError>;
+}
+
+impl<T> MsgVerify for T
+where
+    T: MsgHasSignature + prost::Message,
+{
+    fn verify(&self, verifier: &PublicKey) -> Result<(), MsgVerifyError> {
+        let mut buf = vec![];
+        let msg = self.without_signature();
+
+        msg.encode(&mut buf)?;
+        verifier.verify(&buf, &self.signature())?;
+
+        Ok(())
+    }
+}

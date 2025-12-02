7f494db58eec2be06bd02bb85ccd030e97acecec
Connor McKenzie <connora.mckenzie@gmail.com>
2025-12-02 19:22:51 +0000

    Created errors for MsgVerify & MsgSign

--- helium-proto-crypto/src/lib.rs
+++ helium-proto-crypto/src/lib.rs
@@ -1,5 +1,5 @@
 mod msg_sign;
 mod msg_verify;
 
-pub use msg_sign::MsgSign;
-pub use msg_verify::MsgVerify;
+pub use msg_sign::{MsgSign, MsgSignError};
+pub use msg_verify::{MsgVerify, MsgVerifyError};
--- helium-proto-crypto/src/msg_sign.rs
+++ helium-proto-crypto/src/msg_sign.rs
@@ -1,15 +1,19 @@
 use helium_crypto::{Keypair, Sign};
 use msg_signature::MsgHasSignature;
 
+#[derive(thiserror::Error, Debug)]
+#[error("error signing message: {0}")]
+pub struct MsgSignError(#[from] pub helium_crypto::Error);
+
 pub trait MsgSign {
-    fn sign(&mut self, signing_key: &Keypair) -> Result<(), helium_crypto::Error>;
+    fn sign(&mut self, signing_key: &Keypair) -> Result<(), MsgSignError>;
 }
 
 impl<T> MsgSign for T
 where
     T: MsgHasSignature + prost::Message,
 {
-    fn sign(&mut self, signing_key: &Keypair) -> Result<(), helium_crypto::Error> {
+    fn sign(&mut self, signing_key: &Keypair) -> Result<(), MsgSignError> {
         self.clear_signature();
         let signature = signing_key.sign(&self.encode_to_vec())?;
         self.set_signature(signature);
